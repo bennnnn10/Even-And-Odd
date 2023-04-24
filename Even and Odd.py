c7a408bb3e9de89b42f46a3557f52a5bff67310d
unknown <rv8828120@gmail.com>
2023-04-25 00:42:00 +0800

    Repeat the input file's lines

--- Even and Odd.py
+++ Even and Odd.py
@@ -4,7 +4,10 @@
 
 #Open the "numbers.txt" for reading, and "even.txt" and "odd.text" for appending
 with open("numbers.txt", "r") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
-#Repeat the input file's lines
+    
+    #Repeat the input file's lines
+    for line in numbers_txt_file:
+        numbers = int(line.strip())
 #Check if even
 #If even, write it to the even.txt file
 #If odd, write it to the odd.txt file
