0d4cbe2bd7e495148e810cc7d967d5503ffac92c
unknown <rv8828120@gmail.com>
2023-04-25 00:12:27 +0800

    Pseudocode

--- Even and Odd.py
+++ Even and Odd.py
@@ -1,3 +1,9 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+#Open the "numbers.txt" for reading, and "even.txt" and "odd.text" for appending
+#Repeat the input file's lines
+#Check if even
+#If even, write it to the even.txt file
+#If odd, write it to the odd.txt file
