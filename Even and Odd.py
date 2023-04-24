8b85846b3cbf23bcc5318f0a3c7a2873f0e81be9
unknown <rv8828120@gmail.com>
2023-04-25 00:29:12 +0800

    Open txt files

--- Even and Odd.py
+++ Even and Odd.py
@@ -3,6 +3,7 @@
 #Object-Oriented Programming
 
 #Open the "numbers.txt" for reading, and "even.txt" and "odd.text" for appending
+with open("numbers.txt", "r") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
 #Repeat the input file's lines
 #Check if even
 #If even, write it to the even.txt file
