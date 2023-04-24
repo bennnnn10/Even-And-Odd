4a7d4f633ffa1bc64b4d9ee0ccd060342a183b76
unknown <rv8828120@gmail.com>
2023-04-25 01:04:44 +0800

    numbers txt r

--- Even and Odd.py
+++ Even and Odd.py
@@ -3,8 +3,7 @@
 #Object-Oriented Programming
 
 #Open the "numbers.txt" for reading, and "even.txt" and "odd.text" for appending
-with open("numbers.txt", "r") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
-    
+with open("numbers.txt") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
     #Repeat the input file's lines
     for line in numbers_txt_file:
         numbers = int(line.strip())
