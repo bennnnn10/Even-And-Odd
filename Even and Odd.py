d484830523801e62c2c9b5b2afda7907405b0cc8
unknown <rv8828120@gmail.com>
2023-04-25 10:08:47 +0800

    def process

--- Even and Odd.py
+++ Even and Odd.py
@@ -2,15 +2,17 @@
 #BSCPE 1-4
 #Object-Oriented Programming
 
+def process():
 #Open the "numbers.txt" for reading, and "even.txt" and "odd.text" for appending
-with open("numbers.txt") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
-    #Repeat the input file's lines
-    for line in numbers_txt_file:
-        numbers = int(line.strip())
-        #Check if even
-        if numbers % 2 == 0:
-#If even, write it to the even.txt file
-            even_txt_file.write(f"{numbers}\n")
-        else:
-#If odd, write it to the odd.txt file
-            odd_txt_file.write(f"{numbers}\n")
+    with open("numbers.txt") as numbers_txt_file, open("even.txt", "a") as even_txt_file, open("odd.txt", "a") as odd_txt_file:
+        #Repeat the input file's lines
+        for line in numbers_txt_file:
+            numbers = int(line.strip())
+            #Check if even
+            if numbers % 2 == 0:
+                #If even, write it to the even.txt file
+                even_txt_file.write(f"{numbers}\n")
+            else:
+                #If odd, write it to the odd.txt file
+                odd_txt_file.write(f"{numbers}\n")
+process()
